3d2f7c9f6b2c46a1ebc163ac0f4cd862e543ab49
Roman <lytvynenko.r.i@gmail.com>
2018-04-21 18:58:22 +0300

    fix connection to H2 implementation saveContact in ContactDBImpl

--- src/dao/impl/ContactDBImpl.java
+++ src/dao/impl/ContactDBImpl.java
@@ -7,30 +7,56 @@ import java.sql.*;
 
 public class ContactDBImpl implements ContactDao {
 
-    public static final String URL = "jdbc:h2:tcp://localhost/~/Client";
+    private static final String URL = "jdbc:h2:tcp://localhost/~/Client";
+    private String username = "TEST";
+    private String password = "";
 
     public ContactDBImpl() {
         try {
             Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("Connection");
         }
-        try (Connection connection = DriverManager.getConnection(URL, "Test", "");
-        Statement statement = connection.createStatement()){
-            statement.executeUpdate("CREATE TABLE CLIENTS (ID INT not null  AUTO_INCREMENT, " +
-                    "NAME VARCHAR() not null,  PRIMARY KEY(id);");
-
-//            statement.close();
 
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Connection connection = DriverManager.getConnection(URL, username, password);
+//            Statement statement = connection.createStatement();
+//            statement.executeUpdate("Drop table if exists Clients");
+//            statement.executeUpdate("create table Clients ( id int not null auto_increment, name VARCHAR(30) not null, PRIMARY KEY (id));");
+//            statement.executeUpdate("insert into Clients set name = 'David'");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+
     }
 
     @Override
     public void saveContact(Contact contact) {
-
+        try {
+            Connection connection = DriverManager.getConnection(URL, username, password);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("Drop table if exists Clients");
+            statement.executeUpdate("create table Clients ( id int not null auto_increment, " +
+                    "name VARCHAR(30), " +
+                    "surname VARCHAR(64), " +
+                    "age int, " +
+                    "phoneNumber VARCHAR(13), " +
+                    "PRIMARY KEY (id));");
+//            statement.executeUpdate("insert into Clients set name = '"+ contact.getName() +"'");
+//            statement.executeUpdate("insert into Clients set surname = '"+ contact.getSurname() +"'");
+//            statement.executeUpdate("insert into Clients set age = '"+ contact.getAge() +"'");
+//            statement.executeUpdate("insert into Clients set phoneNumber = '"+ contact.getPhoneNumber() +"'");
+            statement.executeUpdate("insert into clients (name, surname, age, phoneNumber)" +
+                    "values ('" + contact.getName() + "'," +
+                    "'" + contact.getSurname() + "'," +
+                    "'" + contact.getAge() + "'," +
+                    "'" + contact.getPhoneNumber() + "'" +
+                    ");");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/model/Contact.java
+++ src/model/Contact.java
@@ -7,27 +7,30 @@ public class Contact {
     private int age;
     private String phoneNumber;
 
+
+
     public Contact(String name, String surname, int age, String phoneNumber) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.phoneNumber = phoneNumber;
+
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getSurname() {
+        return surname;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     @Override
